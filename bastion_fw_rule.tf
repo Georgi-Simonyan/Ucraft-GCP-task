4eae310419bf44cf71e237c4a8ca8a436dc70f9f
GeorgiSimonyan-DevOps <98820997+GeorgiSimonyan-DevOps@users.noreply.github.com>
2022-04-19 15:09:04 +0400

    Update bastion_fw_rule.tf

--- bastion_fw_rule.tf
+++ bastion_fw_rule.tf
@@ -2,7 +2,7 @@ resource "google_compute_firewall" "bastion_fw" {
   name    = "bastion-firewall"
   network = google_compute_network.main-vpc.name
   # this source range allowing ssh only from your host, input your host public ip
-  source_ranges = ["37.252.82.156/32"] 
+  source_ranges = [""] 
 
   allow {
     protocol = "tcp"
