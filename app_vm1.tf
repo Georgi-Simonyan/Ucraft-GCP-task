889758d403ff9ea8b314c2592b454be2abe23554
Georgi-Simonyan <gigsim97@gmail.com>
2022-04-21 11:12:38 +0400

    fixed

--- app_vm1.tf
+++ app_vm1.tf
@@ -1,8 +1,9 @@
+#Static public ip 
 resource "google_compute_address" "static_ip_app1" {
   name = "juice-shop-0"
   address = "34.159.39.116"
 }
-
+#Static private ip
 resource "google_compute_address" "static_internal_ip_app1" {
   name = "juice-shop-0-internal"
   subnetwork = "app"
@@ -10,7 +11,7 @@ resource "google_compute_address" "static_internal_ip_app1" {
   address = "10.0.0.37"
 }
 
-
+# Creating instance with network and firewall rules assinged
 resource "google_compute_instance" "juice-shop-0" {
   name         = "juice-shop-0"
   machine_type = "e2-medium"
@@ -30,6 +31,6 @@ resource "google_compute_instance" "juice-shop-0" {
       nat_ip = google_compute_address.static_ip_app1.address
     }
   }
-
+  #installing ansible on startup
   metadata_startup_script = "sudo yum install ansible -y" 
 }
